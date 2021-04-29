c346104d59507c15c7664d9d221a54d85527f74a
William Brito <will.oliver.ads@gmail.com>
2021-04-29 15:30:21 -0300

    Commit 23 - Criação de estrutura de arquivos [Criar/Editar/Listar] funcionários

---
+++ front-end-vue/src/components/pages/editar-funcionario/EditarFuncionarioComponent.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="row justify-content-center">
+		<h1>Editar Funcionario</h1>
+	</div>
+</template>
+<script>
+export default {
+	data(){
+		return {
+    	};
+	},
+};
+</script>
