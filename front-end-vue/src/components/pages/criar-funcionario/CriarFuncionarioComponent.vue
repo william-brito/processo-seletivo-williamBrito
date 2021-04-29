e132f760a4df69c3480f591f1dc265792eba8592
William Brito <will.oliver.ads@gmail.com>
2021-04-29 17:39:45 -0300

    Commit 28 - Implementação de validação de campos com o [Vuelidate] no componente [CriarFuncionarioComponent]

--- front-end-vue/src/components/pages/criar-funcionario/CriarFuncionarioComponent.vue
+++ front-end-vue/src/components/pages/criar-funcionario/CriarFuncionarioComponent.vue
@@ -11,10 +11,16 @@
             <label class="font-weight-bold">Nome</label>
             <input
               type="text"
+              id="idNome"
+              name="nNome"
               placeholder="Digite o Nome do Funcionario"
               class="form-control"
               v-model="funcionarioForm.NOME_FUNCIONARIO"
+              :class="{'is-invalid': isSubmitted && $v.funcionarioForm.$error}"
             />
+            <div
+              v-if="isSubmitted && !$v.funcionarioForm.NOME_FUNCIONARIO.required"
+            >O Campo "Nome do Funcionario" nao pode ficar em branco!</div>
           </div>
           <!---- FIM DO BLOCO: Nome do Funcionário ---->
           <!---- INÍCIO DO BLOCO: Setor do Funcionário ---->
@@ -22,10 +28,16 @@
             <label class="font-weight-bold">Setor</label>
             <input
               type="text"
+              id="idSetor"
+              name="nSetor"
               placeholder="Digite o Setor do Funcionario"
               class="form-control"
               v-model="funcionarioForm.NOME_SETOR"
+              :class="{'is-invalid': isSubmitted && $v.funcionarioForm.$error}"
             />
+            <div
+              v-if="isSubmitted && !$v.funcionarioForm.NOME_SETOR.required"
+            >O Campo "Setor do Funcionario" nao pode ficar em branco!</div>
           </div>
           <!---- FIM DO BLOCO: Setor do Funcionário ---->
           <!---- INÍCIO DO BLOCO: Salário do Funcionário ---->
@@ -33,10 +45,16 @@
             <label class="font-weight-bold">Salario</label>
             <input
               type="number"
+              id="idSalario"
+              name="nSalario"
               placeholder="Digite o Salario do Funcionario"
               class="form-control"
               v-model="funcionarioForm.NU_SALARIO"
+              :class="{'is-invalid': isSubmitted && $v.funcionarioForm.$error}"
             />
+            <div
+              v-if="isSubmitted && !$v.funcionarioForm.NU_SALARIO.required"
+            >O Campo "Salario do Funcionario" nao pode ficar em branco!</div>
           </div>
           <!---- FIM DO BLOCO: Salário do Funcionário ---->
           <!---- INÍCIO DO BLOCO: Email do Funcionário ---->
@@ -44,10 +62,16 @@
             <label class="font-weight-bold">Email</label>
             <input
               type="text"
+              id="idEmail"
+              name="nEmail"
               placeholder="Digite o Email do Funcionario"
               class="form-control"
               v-model="funcionarioForm.DS_EMAIL"
+              :class="{'is-invalid': isSubmitted && $v.funcionarioForm.$error}"
             />
+            <div
+              v-if="isSubmitted && !$v.funcionarioForm.DS_EMAIL.required"
+            >O Campo "Email do Funcionario" nao pode ficar em branco!</div>
           </div>
           <!---- FIM DO BLOCO: Email do Funcionário ---->
           <!---- INÍCIO DO BLOCO: Idade do Funcionário ---->
@@ -55,10 +79,16 @@
             <label class="font-weight-bold">Idade</label>
             <input
               type="number"
+              id="idIdade"
+              name="nIdade"
               placeholder="Digite a Idade do Funcionario"
               class="form-control"
               v-model="funcionarioForm.NU_IDADE"
+              :class="{'is-invalid': isSubmitted && $v.funcionarioForm.$error}"
             />
+            <div
+              v-if="isSubmitted && !$v.funcionarioForm.NU_IDADE.required"
+            >O Campo "Idade do Funcionario" nao pode ficar em branco!</div>
           </div>
           <!---- FIM DO BLOCO: Idade do Funcionário ---->
           <div class="form-group">
@@ -69,7 +99,9 @@
     </div>
   </div>
   </template>
- <script>
+<script>
+import { required } from 'vuelidate/lib/validators';
+
 export default {
   data() {
     return {
@@ -80,10 +112,35 @@ export default {
         DS_EMAIL: '',
         NU_IDADE: '',
       },
+      isSubmitted: false,
     };
+  },
+  validations: {
+    funcionarioForm: {
+      NOME_FUNCIONARIO: {
+        required,
+      },
+      NOME_SETOR: {
+        required,
+      },
+      NU_SALARIO: {
+        required,
+      },
+      DS_EMAIL: {
+        required,
+      },
+      NU_IDADE: {
+        required,
+      },
+    },
   },
   methods: {
     handleSubmitForm() {
+      this.isSubmitted = true;
+      this.$v.$touch();
+      if (this.$v.invalid) {
+        return;
+      }
     },
   },
 };
--- front-end-vue/src/main.js
+++ front-end-vue/src/main.js
@@ -1,4 +1,5 @@
 import VueSweetalert2 from 'vue-sweetalert2';
+import Vuelidate from 'vuelidate';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
@@ -7,6 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 Vue.config.productionTip = false;
 Vue.use(VueSweetalert2);
+Vue.use(Vuelidate);
 
 new Vue({
   router,
